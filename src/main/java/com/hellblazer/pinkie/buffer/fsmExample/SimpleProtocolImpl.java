15026e5bc64969337a8629b9969e26cf015c1fdf
Hellblazer <hellblazer@me.com>
2013-10-29 08:38:11 -0700

    Fix transition error in sendmessage client state.  log level back to info.  Copyright notices

--- src/main/java/com/hellblazer/pinkie/buffer/fsmExample/SimpleProtocolImpl.java
+++ src/main/java/com/hellblazer/pinkie/buffer/fsmExample/SimpleProtocolImpl.java
@@ -31,320 +31,319 @@ import com.hellblazer.utils.Gate;
  * 
  */
 public class SimpleProtocolImpl implements SimpleProtocol {
-	private class BufferProtocolHandlerImpl implements BufferProtocolHandler {
+    private class BufferProtocolHandlerImpl implements BufferProtocolHandler {
 
-		@Override
-		public void accepted(BufferProtocol bufferProtocol) {
-			fsm.accepted(bufferProtocol);
-		}
+        @Override
+        public void accepted(BufferProtocol bufferProtocol) {
+            fsm.accepted(bufferProtocol);
+        }
 
-		@Override
-		public void closing() {
-			fsm.closing();
-		}
+        @Override
+        public void closing() {
+            fsm.closing();
+        }
 
-		@Override
-		public void connected(BufferProtocol bufferProtocol) {
-			fsm.connected(bufferProtocol);
-		}
+        @Override
+        public void connected(BufferProtocol bufferProtocol) {
+            fsm.connected(bufferProtocol);
+        }
 
-		@Override
-		public ByteBuffer newReadBuffer() {
-			return ByteBuffer.allocate(1024);
-		}
+        @Override
+        public ByteBuffer newReadBuffer() {
+            return ByteBuffer.allocate(1024);
+        }
 
-		@Override
-		public ByteBuffer newWriteBuffer() {
-			return ByteBuffer.allocate(1024);
-		}
+        @Override
+        public ByteBuffer newWriteBuffer() {
+            return ByteBuffer.allocate(1024);
+        }
 
-		@Override
-		public void readError() {
-			fsm.readError();
-		}
+        @Override
+        public void readError() {
+            fsm.readError();
+        }
 
-		@Override
-		public void readReady() {
-			fsm.readReady();
-		}
+        @Override
+        public void readReady() {
+            fsm.readReady();
+        }
 
-		@Override
-		public void writeError() {
-			fsm.writeError();
-		}
+        @Override
+        public void writeError() {
+            fsm.writeError();
+        }
 
-		@Override
-		public void writeReady() {
-			fsm.writeReady();
-		}
-	}
-	
-	public static interface MessageHandler {
-		
-		void handle(String message);
-	}
+        @Override
+        public void writeReady() {
+            fsm.writeReady();
+        }
+    }
 
-	private final BufferProtocolHandlerImpl handler;
-	private final SimpleProtocolContext fsm;
+    public static interface MessageHandler {
 
-	private BufferProtocol bufferProtocol;
+        void handle(String message);
+    }
 
-	private static final Logger LOG = LoggerFactory
-			.getLogger(SimpleProtocolImpl.class);
+    private final BufferProtocolHandlerImpl handler;
+    private final SimpleProtocolContext     fsm;
 
-	private Gate sendGate;
-	
-	private final MessageHandler messageHandler;
+    private BufferProtocol                  bufferProtocol;
 
-	public SimpleProtocolImpl(MessageHandler messageHandler) {
-		fsm = new SimpleProtocolContext(this);
-		handler = new BufferProtocolHandlerImpl();
-		sendGate = new Gate();
-		this.messageHandler = messageHandler;
-	}
+    private static final Logger             LOG = LoggerFactory.getLogger(SimpleProtocolImpl.class);
 
-	public void send(String msg) {
-		try {
-			sendGate.await();
-			fsm.transmitMessage(msg);
-		} catch (InterruptedException e) {
-			return;
-		}
+    private Gate                            sendGate;
 
-	}
+    private final MessageHandler            messageHandler;
 
-	@Override
-	public void setBufferProtocol(BufferProtocol bp, String fsmName) {
-		bufferProtocol = bp;
-		fsm.setName(fsmName);
-	}
+    public SimpleProtocolImpl(MessageHandler messageHandler) {
+        fsm = new SimpleProtocolContext(this);
+        handler = new BufferProtocolHandlerImpl();
+        sendGate = new Gate();
+        this.messageHandler = messageHandler;
+    }
 
-	public BufferProtocolHandler getBufferProtocolHandler() {
-		return handler;
-	}
+    public void send(String msg) {
+        try {
+            sendGate.await();
+            fsm.transmitMessage(msg);
+        } catch (InterruptedException e) {
+            return;
+        }
 
-	@Override
-	public void establishClientSession() {
-		ByteBuffer buffer = bufferProtocol.getWriteBuffer();
-		buffer.clear();
-		buffer.put((byte) MessageType.ESTABLISH.ordinal());
-		buffer.flip();
-		ByteBuffer readBuffer = bufferProtocol.getReadBuffer();
-		readBuffer.limit(1);
-		readBuffer.rewind();
-		bufferProtocol.selectForWrite();
+    }
 
-	}
+    @Override
+    public void setBufferProtocol(BufferProtocol bp, String fsmName) {
+        bufferProtocol = bp;
+        fsm.setName(fsmName);
+    }
 
-	@Override
-	public void ackReceived() {
-		ByteBuffer readBuffer = bufferProtocol.getReadBuffer();
-		readBuffer.rewind();
-		byte type = readBuffer.get();
-		
-		if (type == (byte) MessageType.ACK.ordinal()) {
-			fsm.ackReceived();
-		} else {
-			LOG.error("Message type should be ACK but is {} instead",
-					MessageType.values()[type]);
-			fsm.protocolError();
-		}
+    public BufferProtocolHandler getBufferProtocolHandler() {
+        return handler;
+    }
 
-	}
+    @Override
+    public void establishClientSession() {
+        ByteBuffer buffer = bufferProtocol.getWriteBuffer();
+        buffer.clear();
+        buffer.put((byte) MessageType.ESTABLISH.ordinal());
+        buffer.flip();
+        ByteBuffer readBuffer = bufferProtocol.getReadBuffer();
+        readBuffer.limit(1);
+        readBuffer.rewind();
+        bufferProtocol.selectForWrite();
 
-	/**
-	 * @return
-	 */
-	public SimpleProtocolState getCurrentState() {
-		return fsm.getState();
-	}
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.hellblazer.pinkie.buffer.fsmExample.SimpleProtocol#sendMessage(com
-	 * .hellblazer.pinkie.buffer.fsmExample.Message)
-	 */
-	@Override
-	public void transmitMessage(String message) {
-		ByteBuffer buffer = bufferProtocol.getWriteBuffer();
-		buffer.rewind();
-		buffer.limit(message.length() + 2);
-		buffer.put((byte) MessageType.MSG.ordinal());
-		bufferProtocol.setReadFullBuffer(false);
-		buffer.put((byte) message.length());
-		buffer.put(message.getBytes());
-		buffer.flip();
-		bufferProtocol.selectForWrite();
+    @Override
+    public void ackReceived() {
+        ByteBuffer readBuffer = bufferProtocol.getReadBuffer();
+        readBuffer.rewind();
+        byte type = readBuffer.get();
 
-		// clear the buffer of all the old crap
-		ByteBuffer readBuffer = bufferProtocol.getReadBuffer();
-		readBuffer.limit(258);
-		bufferProtocol.selectForRead();
+        if (type == (byte) MessageType.ACK.ordinal()) {
+            fsm.ackReceived();
+        } else {
+            LOG.error("Message type should be ACK but is {} instead",
+                      MessageType.values()[type]);
+            fsm.protocolError();
+        }
 
-	}
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.hellblazer.pinkie.buffer.fsmExample.SimpleProtocol#sendGoodbye()
-	 */
-	@Override
-	public void sendGoodbye() {
-		ByteBuffer buffer = bufferProtocol.getWriteBuffer();
-		buffer.put((byte) MessageType.GOOD_BYE.ordinal());
-		buffer.flip();
+    /**
+     * @return
+     */
+    public SimpleProtocolState getCurrentState() {
+        return fsm.getState();
+    }
 
-		bufferProtocol.selectForWrite();
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.hellblazer.pinkie.buffer.fsmExample.SimpleProtocol#sendMessage(com
+     * .hellblazer.pinkie.buffer.fsmExample.Message)
+     */
+    @Override
+    public void transmitMessage(String message) {
+        ByteBuffer buffer = bufferProtocol.getWriteBuffer();
+        buffer.rewind();
+        buffer.limit(message.length() + 2);
+        buffer.put((byte) MessageType.MSG.ordinal());
+        bufferProtocol.setReadFullBuffer(false);
+        buffer.put((byte) message.length());
+        buffer.put(message.getBytes());
+        buffer.flip();
+        bufferProtocol.selectForWrite();
 
-	public void close() {
-		fsm.close();
-	}
+        // clear the buffer of all the old crap
+        ByteBuffer readBuffer = bufferProtocol.getReadBuffer();
+        readBuffer.limit(258);
+        bufferProtocol.selectForRead();
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.hellblazer.pinkie.buffer.fsmExample.SimpleProtocol#enableSend()
-	 */
-	@Override
-	public void enableSend() {
-		sendGate.open();
-	}
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.hellblazer.pinkie.buffer.fsmExample.SimpleProtocol#establishServerSession
-	 * ()
-	 */
-	@Override
-	public void establishServerSession() {
-		ByteBuffer readBuffer = bufferProtocol.getReadBuffer();
-		readBuffer.rewind();
-		byte type = readBuffer.get();
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.hellblazer.pinkie.buffer.fsmExample.SimpleProtocol#sendGoodbye()
+     */
+    @Override
+    public void sendGoodbye() {
+        ByteBuffer buffer = bufferProtocol.getWriteBuffer();
+        buffer.put((byte) MessageType.GOOD_BYE.ordinal());
+        buffer.flip();
 
-		if (type != (byte) MessageType.ESTABLISH.ordinal()) {
-			LOG.error("Message type should be ESTABLISH but is {} instead",
-					MessageType.values()[type]);
-			fsm.protocolError();
-			return;
-		}
-		ByteBuffer buffer = bufferProtocol.getWriteBuffer();
-		buffer.rewind();
-		buffer.limit(1);
-		buffer.put((byte) MessageType.ACK.ordinal());
-		bufferProtocol.setReadFullBuffer(false);
-		buffer.flip();
-		bufferProtocol.selectForWrite();
+        bufferProtocol.selectForWrite();
+    }
 
-	}
+    public void close() {
+        fsm.close();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.hellblazer.pinkie.buffer.fsmExample.SimpleProtocol#awaitSession()
-	 */
-	@Override
-	public void awaitSession() {
-		ByteBuffer readBuffer = bufferProtocol.getReadBuffer();
-		readBuffer.limit(1);
-		readBuffer.rewind();
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.hellblazer.pinkie.buffer.fsmExample.SimpleProtocol#enableSend()
+     */
+    @Override
+    public void enableSend() {
+        sendGate.open();
+    }
 
-		// waiting for stuff to read...
-		bufferProtocol.selectForRead();
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.hellblazer.pinkie.buffer.fsmExample.SimpleProtocol#establishServerSession
+     * ()
+     */
+    @Override
+    public void establishServerSession() {
+        ByteBuffer readBuffer = bufferProtocol.getReadBuffer();
+        readBuffer.rewind();
+        byte type = readBuffer.get();
 
-	}
+        if (type != (byte) MessageType.ESTABLISH.ordinal()) {
+            LOG.error("Message type should be ESTABLISH but is {} instead",
+                      MessageType.values()[type]);
+            fsm.protocolError();
+            return;
+        }
+        ByteBuffer buffer = bufferProtocol.getWriteBuffer();
+        buffer.rewind();
+        buffer.limit(1);
+        buffer.put((byte) MessageType.ACK.ordinal());
+        bufferProtocol.setReadFullBuffer(false);
+        buffer.flip();
+        bufferProtocol.selectForWrite();
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.hellblazer.pinkie.buffer.fsmExample.SimpleProtocol#processMessage()
-	 */
-	@Override
-	public void processMessage() {
-		ByteBuffer readBuffer = bufferProtocol.getReadBuffer();
-		readBuffer.mark();
-		readBuffer.position(0);
-		byte type = readBuffer.get(0);
+    }
 
-		if (type == (byte) MessageType.MSG.ordinal()) {
-			readMessage(readBuffer);
-		} else if (type == (byte) MessageType.GOOD_BYE.ordinal()) {
-			fsm.goodBye();
-		} else {
-			LOG.error(
-					"Message type should be MSG or GOOD_BYE but is {} instead",
-					MessageType.values()[type]);
-			fsm.protocolError();
-			return;
-		}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.hellblazer.pinkie.buffer.fsmExample.SimpleProtocol#awaitSession()
+     */
+    @Override
+    public void awaitSession() {
+        ByteBuffer readBuffer = bufferProtocol.getReadBuffer();
+        readBuffer.limit(1);
+        readBuffer.rewind();
 
-	}
+        // waiting for stuff to read...
+        bufferProtocol.selectForRead();
 
-	/**
-	 * @param readBuffer
-	 */
-	private void readMessage(ByteBuffer readBuffer) {
-		if (readBuffer.limit() < 2) {
-			readBuffer.reset();
-			bufferProtocol.selectForRead();
-			return;
-		}
-		byte size = readBuffer.get(1);
+    }
 
-		// if the limit is less than the size, we still have
-		// to wait for more of the message
-		if (readBuffer.limit() < size + 2) {
-			readBuffer.reset();
-			bufferProtocol.selectForRead();
-			return;
-		}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.hellblazer.pinkie.buffer.fsmExample.SimpleProtocol#processMessage()
+     */
+    @Override
+    public void processMessage() {
+        ByteBuffer readBuffer = bufferProtocol.getReadBuffer();
+        readBuffer.mark();
+        readBuffer.position(0);
+        byte type = readBuffer.get(0);
 
-		byte[] message = new byte[size];
-		readBuffer.position(2);
-		readBuffer.get(message);
+        if (type == (byte) MessageType.MSG.ordinal()) {
+            readMessage(readBuffer);
+        } else if (type == (byte) MessageType.GOOD_BYE.ordinal()) {
+            fsm.goodBye();
+        } else {
+            LOG.error("Message type should be MSG or GOOD_BYE but is {} instead",
+                      MessageType.values()[type]);
+            fsm.protocolError();
+            return;
+        }
 
-		readBuffer.limit(0);
-		readBuffer.rewind();
+    }
 
-		String msg = new String(message);
-		messageHandler.handle(msg);
-		fsm.messageProcessed();
-	}
+    /**
+     * @param readBuffer
+     */
+    private void readMessage(ByteBuffer readBuffer) {
+        if (readBuffer.limit() < 2) {
+            readBuffer.reset();
+            bufferProtocol.selectForRead();
+            return;
+        }
+        byte size = readBuffer.get(1);
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.hellblazer.pinkie.buffer.fsmExample.SimpleProtocol#awaitMessage()
-	 */
-	@Override
-	public void awaitMessage() {
-		ByteBuffer readBuffer = bufferProtocol.getReadBuffer();
-		readBuffer.clear();
-		bufferProtocol.selectForRead();
-	}
-	
-	@Override
-	public void logProtocolError(String message) {
-		LOG.error("PROTOCOL ERROR: Transition: {}", message);
-	}
+        // if the limit is less than the size, we still have
+        // to wait for more of the message
+        if (readBuffer.limit() < size + 2) {
+            readBuffer.reset();
+            bufferProtocol.selectForRead();
+            return;
+        }
 
-	/* (non-Javadoc)
-	 * @see com.hellblazer.pinkie.buffer.fsmExample.SimpleProtocol#awaitAck()
-	 */
-	@Override
-	public void awaitAck() {
-		ByteBuffer readBuffer = bufferProtocol.getReadBuffer();
-		readBuffer.clear();
-		readBuffer.limit(1);
-		bufferProtocol.selectForRead();
-		
-	}
+        byte[] message = new byte[size];
+        readBuffer.position(2);
+        readBuffer.get(message);
+
+        readBuffer.limit(0);
+        readBuffer.rewind();
+
+        String msg = new String(message);
+        messageHandler.handle(msg);
+        fsm.messageProcessed();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.hellblazer.pinkie.buffer.fsmExample.SimpleProtocol#awaitMessage()
+     */
+    @Override
+    public void awaitMessage() {
+        ByteBuffer readBuffer = bufferProtocol.getReadBuffer();
+        readBuffer.clear();
+        bufferProtocol.selectForRead();
+    }
+
+    @Override
+    public void logProtocolError(String message) {
+        LOG.error("PROTOCOL ERROR: Transition: {}, FSM: {}", message,
+                  fsm.getName());
+    }
+
+    /* (non-Javadoc)
+     * @see com.hellblazer.pinkie.buffer.fsmExample.SimpleProtocol#awaitAck()
+     */
+    @Override
+    public void awaitAck() {
+        ByteBuffer readBuffer = bufferProtocol.getReadBuffer();
+        readBuffer.clear();
+        readBuffer.limit(1);
+        bufferProtocol.selectForRead();
+
+    }
 
 }
